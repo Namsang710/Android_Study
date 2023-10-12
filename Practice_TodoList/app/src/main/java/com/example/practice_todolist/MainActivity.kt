f1a6679094fd81897632a6575d263511daa927fb
Namsang710 <nyh710@gmail.com>
2023-10-12 23:59:38 +0900

    [23-10-12] Update MainActivity

--- Practice_TodoList/app/src/main/java/com/example/practice_todolist/MainActivity.kt
+++ Practice_TodoList/app/src/main/java/com/example/practice_todolist/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.practice_todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.practice_todolist.adapter.TaskAdapter
 import com.example.practice_todolist.databinding.ActivityMainBinding
+import com.example.practice_todolist.viewmodel.TaskViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater)}
+    private val viewModel by viewModels<TaskViewModel>()
+    private var taskAdapter: TaskAdapter? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -14,7 +20,20 @@ class MainActivity : AppCompatActivity() {
 
 
     private fun initUI() {
+        taskAdapter = TaskAdapter(
+            taskFinishEvent = {task -> viewModel.finishTask(task)},
+            taskDeleteEvent = {task -> viewModel.deleteTask(task)}
+        )
 
+        binding.btnAdd.setOnClickListener{
+
+        }
+
+        with(binding.rvTask){
+            setHasFixedSize(true) // 리사이클러뷰 아이템 크기 고정
+            adapter = taskAdapter
+            layoutManager = LinearLayoutManager(this@MainActivity)
+        }
 
 
     }
