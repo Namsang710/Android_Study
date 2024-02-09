9b89b7e3c78cfc64e9bb9548875f4aa98419a430
Namsang710 <nyh710@gmail.com>
2024-02-09 23:05:25 +0900

    24-02-09
    Kotlin 기본 문법

---
+++ app/src/main/java/com/example/practice_kotlin/Example2.kt
@@ -0,0 +1,26 @@
+package com.example.practice_kotlin
+
+fun main(){
+    val result = test(1, 3, c = 5)
+    println(result)
+    test2(id = "윤형님", name = "남윤형", nickname = "윤형")
+    println(times(1, 3))
+    println(times2(1, 3))
+
+}
+
+
+// 2. 함수
+fun test(a : Int, b : Int = 3, c : Int = 4): Int{
+    println(a + b)
+    return a + b
+}
+
+
+fun test2(name: String, nickname : String, id: String) = println(name + nickname + id)
+
+fun times(a : Int, b : Int): Int {
+    return a * b
+}
+
+fun times2(a : Int, b : Int): Int = a * b
